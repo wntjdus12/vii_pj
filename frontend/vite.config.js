d1d1181f1ef2186b244d28e3d7ddd556a162997b
wntjdus12 <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>
2025-06-20 05:05:33 +0000

    sdasa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // 외부 접속 허용
+    port: 5173, // 명시적으로 포트 설정 (선택)
+    proxy: {
+      '/uploadFile': {
+        target: 'http://43.201.168.127:5000', // ← 당신의 백엔드 주소
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
